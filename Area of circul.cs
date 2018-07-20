0afb9ab6d0760894d7f4d5fc5de7c0573d2ee6e6
Matt s <41280405+rand0mboss@users.noreply.github.com>
2018-07-20 13:26:30 -0700

    Add files via upload
    The following program demonstrates defining and using a constant in your program. Finds area of circle.

---
+++ Area of circul.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Radius
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14159;
+            // constant declaration find area 
+            double r;
+            Console.WriteLine("Radius?: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            double areaCircle = pi * r * r;
+            //output
+            Console.WriteLine("Radius: {0}, Area: {1}", r, areaCircle);
+            Console.ReadLine();
+        }
+    }
+}
