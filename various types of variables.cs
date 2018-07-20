9fea0bb2fabdcae6981739415aee262998df0001
Matt s <41280405+rand0mboss@users.noreply.github.com>
2018-07-20 13:18:02 -0700

    Add files via upload
     initialize variables properly, otherwise sometimes program may produce unexpected result.

---
+++ various types of variables.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // List 
+            short a;
+            int b;
+            double c;
+            
+            // Values
+            a = 10;
+            b = 20;
+            c = a + b;
+
+            // Output
+            Console.WriteLine("a = {0}, b = {1}, c = {2}", a, b, c);
+            Console.ReadLine();
+        }
+    }
+}
