11369b50422ac2d73e3a7a66721c44777a4796b3
Matt s <41280405+rand0mboss@users.noreply.github.com>
2018-07-20 13:13:12 -0700

    Add files via upload
    C# Type Conversion Method

---
+++ C# Type Conversion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversionApplication
+{
+    class StringConversion
+    {
+        static void Main(string[] args)
+        {
+            //Varibles 
+            int i = 85;
+            float f = 43.005f;
+            double d = 28845.7652;
+            bool b = true;
+            // C# Type Conversion
+            Console.WriteLine(i.ToString());
+            Console.WriteLine(f.ToString());
+            Console.WriteLine(d.ToString());
+            Console.WriteLine(b.ToString());
+            Console.ReadKey();
+        }
+    }
+}
